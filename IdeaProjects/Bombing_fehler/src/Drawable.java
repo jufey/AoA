ae712a20c445222b7742acf20f30f9fa6ab88a4e
JuFey <justin.marks@uni-duesseldorf.de>
2014-04-20 23:43:59 +0200

    Andere Ordner entfernt

--- IdeaProjects/Bombing_fehler/src/Drawable.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by jufey on 18.04.14.
- */
-import java.awt.Graphics;
-public interface Drawable {
-
-    public void drawObjects(Graphics g);
-}
--- IdeaProjects/Bombing_fehler/src/Movable.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by jufey on 18.04.14.
- */
-public interface Movable {
-    public void doLogic(long delta);
-
-    public void move(long delta);
-}
